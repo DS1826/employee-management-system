81574ded499646bf1ef4b219e359c50b1fad0260
Dina Saunders <dina_saunders@mac.com>
2020-11-04 21:58:22 -0500

    correct mysql connection error

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "MArion19",
+    password: "BCSds1826$!",
     database: "employee_tracker_db"
 });
 
