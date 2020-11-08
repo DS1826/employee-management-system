44ff15d86be54de57233b6e7e6138488d380a322
Dina Saunders <dina_saunders@mac.com>
2020-11-08 14:46:22 -0500

    added manager column to view all employee query result

--- app.js
+++ app.js
@@ -147,7 +147,7 @@ function getManagers() {
 
 // View All Employees
 function viewEmployees() {
-    let query = "SELECT first_name, last_name, title, department.name AS department, salary FROM employee INNER JOIN role ON employee.role_id = role.id INNER JOIN department ON role.department_id = department.id"
+    let query = "SELECT CONCAT(employee.first_name,' ',employee.last_name) AS name, title, department.name AS department, salary, CONCAT(e.first_name,' ',e.last_name) AS manager FROM employee INNER JOIN role ON employee.role_id = role.id INNER JOIN department ON role.department_id = department.id LEFT JOIN employee e ON employee.manager_id = e.id;"
 
     connection.query(query, function (err, res) {
             if (err) throw err;
